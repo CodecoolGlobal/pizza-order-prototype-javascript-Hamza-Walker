f488797a3e79e018bbeeaf846bf9db75849288b0
hamza-walker <hamzawalker@Hamzas-MacBook-Pro.local>
2023-04-03 12:56:46 +0200

    added json files and installed packages SET UP

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+
+
+
+
+const port = 9001;
+app.listen(port, () => console.log(`http://127.0.0.1:${port}`));
