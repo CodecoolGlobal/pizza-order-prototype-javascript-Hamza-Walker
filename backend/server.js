899bd92060afba68f10247c7ba661b114d3f8caa
hamza-walker <hamzawalker@Hamzas-MacBook-Pro.local>
2023-04-04 10:21:09 +0200

    some changes

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
 
+app.get('/', (req, res) => {
+    res.redirect(301, '/index.html');
+  });
+  
 app.use('/api', apiRoutes);
 
 app.listen(port, () => console.log(`http://127.0.0.1:${port}`));
