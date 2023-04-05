9a3a6798987b1dd280d2ff674bcd5ba1eb08ad18
DanielB18 <daniel182612@gmail.com>
2023-04-05 04:41:31 +0200

    added the allergens and pizza json files and made the routes

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-import config from "./app/config.js"
-import express from "express"
+import config from './app/config.js';
+import express from 'express';
+import rootRoute from './routes/root.route.js';
+import fs from 'node:fs/promises';
 
-import rootRoute from "./routes/root.route.js"
+const server = express();
 
-const server = express()
+server.use(express.static('public'));
+server.use('/', rootRoute);
 
-server.use(express.static("public"))
-server.use("/", rootRoute)
+async function readJSONFile(filePath) {
+  const data = await fs.readFile(filePath);
+  return JSON.parse(data);
+}
 
-server.listen(config.api.port, () => console.log(`Server listening on http://localhost:${config.api.port}`))
+server.get('/api/pizza', async (req, res) => {
+  const pizzas = await readJSONFile('./data/pizzas.json');
+  res.json(pizzas);
+});
+
+server.get('/api/allergen', async (req, res) => {
+  const allergens = await readJSONFile('./data/allergens.json');
+  res.json(allergens);
+});
+
+server.listen(config.api.port, () => console.log(`Server listening on http://localhost:${config.api.port}`));
