a5229925300b7ad250501e1c0588906137f84a92
ABitOfAByte <marcus.migotti@gmail.com>
2023-04-06 21:14:06 +0200

    added a few more validators

--- app/middleware/validators.js
+++ app/middleware/validators.js
@@ -1,6 +1,6 @@
 import { all, get } from "../database.js"
 
-export function uuidValidator(uuid) {
+export function isValidUuid(uuid) {
 	if (!/[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/.test(uuid))
 		throw { status: 400, message: "Malformed UUID", data: { id: uuid } }
 }
@@ -10,6 +10,16 @@ export function keyExistsValidator(table, id) {
 }
 
 export function nameIsUniqueValidator(table, name) {
-	if (all(table).find((item) => item.name === name))
+	if (all(table).find(item => item.name === name))
 		throw { status: 400, message: "Name already exists", data: { name } }
+}
+
+export function isNotEmptyString(name, value) {
+	if (value === "")
+		throw { status: 400, message: `Required field "${name}" can not be empty.`, data: { fieldName: name } }
+}
+
+export function isValidEmail(email) {
+	if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email))
+		throw { status: 400, message: "Malformed e-mail address", data: { email } }
 }
