2c47087664ffbee895e9ae2b4f49899a05fc56b2
ABitOfAByte <marcus.migotti@gmail.com>
2023-04-06 21:14:21 +0200

    added validation

--- routes/orders.route.js
+++ routes/orders.route.js
@@ -1,4 +1,5 @@
 import dataBaseRouter from "../app/middleware/dataRouting.js"
+import { isNotEmptyString, isValidEmail, isValidUuid } from "../app/middleware/validators.js"
 
 export default dataBaseRouter("orders", {
 	validation: {
@@ -9,11 +10,23 @@ export default dataBaseRouter("orders", {
 })
 
 function postValidator(req, res, next) {
+	const { name, email, phone, address } = req.body.customerInfo
+
+	console.log(req.body)
+
+	isNotEmptyString("name", name)
+	isNotEmptyString("email", email)
+	isValidEmail(email)
+	isNotEmptyString("phone", phone)
+	isNotEmptyString("address", address)
+
 	next()
 }
 function putValidator(req, res, next) {
+	isValidUuid(req.params.id)
 	next()
 }
 function deleteValidator(req, res, next) {
+	isValidUuid(req.params.id)
 	next()
 }
