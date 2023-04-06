0b3ac38c6bcf70a43191909dd27871e97a3f549d
hamza-walker <hamzawalker@Hamzas-MacBook-Pro.local>
2023-04-06 10:30:18 +0200

    skr skrrr

--- public/helpScript.js
+++ public/helpScript.js
@@ -2,51 +2,18 @@ import { firstDiv, secondDiv, thirdDiv} from "./script.js";
 export let selectedElement = null;
 let pizzaDivs; 
 
-
-// export const createFirstDivElements = (pizzaObject) => {
-//   const pizzaDivs = pizzaObject.map(pizza => {
-//     const pizzaDiv = document.createElement("div");
-//     pizzaDiv.classList.add("pizza-item");
-
-//     const pizzaName = document.createElement("h3");
-//     pizzaName.textContent = pizza.name;
-//     pizzaDiv.appendChild(pizzaName);
-
-//     const pizzaPrice = document.createElement("p");
-//     pizzaPrice.textContent = `Price: $${pizza.price}`;
-//     pizzaDiv.appendChild(pizzaPrice);
-
-//     // Add click event listener to the pizza div
-//     pizzaDiv.addEventListener('click', () => {
-//       selectedElement = pizza;
-//       console.log('Selected pizza:', selectedElement);
-//       const existingDescriptionDiv = document.querySelector('.description');
-//       if (existingDescriptionDiv) {
-//         existingDescriptionDiv.remove();
-//       }
-//       const descriptionDiv = createSecondDivElements(pizzaObject);
-//       const pizzaContainer = document.querySelector('.pizza-container');
-//       if (pizzaContainer) {
-//         pizzaContainer.appendChild(descriptionDiv);
-//       }
-//     });
-
-//     return pizzaDiv;
-//   });
-//   return pizzaDivs;
-// };
-
-
 export const createFirstDivElements = (pizzaData, allergensData) => {
   // Assign the pizza divs to the `pizzaDivs` variable declared outside the function
     pizzaDivs = pizzaData.map(pizza => {
     const pizzaDiv = document.createElement("div");
     pizzaDiv.classList.add('pizza-item');
     pizzaDiv.classList.add(pizza.name.toLowerCase().replace(/\s+/g, '-'));
+    pizzaDiv.classList.add('hover-effect');
 
     const pizzaName = document.createElement("h3");
     pizzaName.textContent = pizza.name;
     pizzaDiv.appendChild(pizzaName);
+
 
     const pizzaPrice = document.createElement("p");
     pizzaPrice.textContent = `Price: $${pizza.price}`;
@@ -102,34 +69,36 @@ export const createAllergensFilter = (pizzaData, allergensData) => {
 
   mainDiv.appendChild(allergenCheckboxes);
 
-  // Add event listener to the allergen checkboxes
-  allergenCheckboxes.addEventListener('change', () => {
-    const checkedAllergens = Array.from(
-      allergenCheckboxes.querySelectorAll('input:checked')
-    ).map((checkbox) => checkbox.dataset.allergenId);
-      console.log(allergenCheckboxes)
-  
-    pizzaDivs.forEach((pizzaDiv) => {
-      const pizzaName = pizzaDiv.classList[1]; // Assumes the second class in the classList contains the pizza name
-  
-      // Get the corresponding pizza object
-      const pizza = pizzaData.find(p => p.name.toLowerCase().replace(/\s+/g, '-') === pizzaName);
-  
-      const pizzaAllergens = allergensData.filter(allergen => pizza.allergens.includes(allergen.id));
-  
-      console.log(pizzaAllergens)
-  
-      const hasAllCheckedAllergens = checkedAllergens.every(checkedAllergen => !pizzaAllergens.find(allergen => allergen.id === checkedAllergen));
-  
-      if (hasAllCheckedAllergens) {
-        pizzaDiv.classList.add('hover-effect');
-      } else {
-        pizzaDiv.classList.remove('hover-effect');
-      }
-    });
-  });
 
-  thirdDiv.appendChild(mainDiv);
+// Add event listener to the allergen checkboxes
+allergenCheckboxes.addEventListener('change', () => {
+  const checkedAllergens = Array.from(
+    allergenCheckboxes.querySelectorAll('input:checked')
+  ).map((checkbox) => checkbox.dataset.allergenId);
+    console.log(allergenCheckboxes)
+
+  pizzaDivs.forEach((pizzaDiv) => {
+    const pizzaName = pizzaDiv.classList[1]; // Assumes the second class in the classList contains the pizza name
+
+    // Get the corresponding pizza object
+    const pizza = pizzaData.find(p => p.name.toLowerCase().replace(/\s+/g, '-') === pizzaName);
+
+    const pizzaAllergens = allergensData.filter(allergen => pizza.allergens.includes(allergen.id));
+
+    console.log(pizzaAllergens)
+
+    const hasAllCheckedAllergens = checkedAllergens.every(checkedAllergen => !pizzaAllergens.find(allergen => allergen.id === checkedAllergen));
+
+    if (hasAllCheckedAllergens) {
+      pizzaDiv.classList.add('hover-effect');
+    } else {
+      pizzaDiv.classList.remove('hover-effect');
+    }
+  });
+});
+
+  
+  thirdDiv.appendChild(mainDiv)
   return mainDiv;
 };
 
@@ -139,7 +108,7 @@ export const createSecondDivElements = (pizzaObject) => {
   const descriptionDiv = document.createElement('div');
   descriptionDiv.classList.add('description');
 
-  // create img div
+
   const imgDiv = document.createElement('div');
   imgDiv.classList.add('description__img');
 
@@ -149,7 +118,6 @@ export const createSecondDivElements = (pizzaObject) => {
 
   imgDiv.appendChild(img);
 
-  // create pizza description div
   const pizzaDescDiv = document.createElement('div');
   pizzaDescDiv.classList.add('description__pizza-desc');
 
@@ -168,29 +136,21 @@ export const createSecondDivElements = (pizzaObject) => {
   pizzaDescDiv.appendChild(pizzaDescription);
 
   function createOrderPizzaButton() {
-    // create div element
     var container = document.createElement("div");
-    // create button element
     var pizzaButton = document.createElement("button");
   
-    // set button text
     pizzaButton.innerHTML = "🔥 Into The Oven 🔥";
   
-    // set button class
     pizzaButton.classList.add("pizza-button")
     pizzaButton.addEventListener('click', function() {
-      // intoTheOven(selectedElement)
       createthirdDivElements(selectedElement)
     });
-    // add button to div
     container.appendChild(pizzaButton);
-    // return div
     return container;
   }
   const intoTheOven = createOrderPizzaButton()
   pizzaDescDiv.appendChild(intoTheOven);
 
-  // create ingredients div
   const ingredientsDiv = document.createElement('div');
   ingredientsDiv.classList.add('description__ingredients');
 
@@ -200,7 +160,6 @@ export const createSecondDivElements = (pizzaObject) => {
   const ingredientsList = document.createElement('ul');
 
   selectedElementPizza.ingredients.forEach((ingredient) => {
-    // console.log(ingredient)
     const ingredientItem = document.createElement('li');
     ingredientItem.textContent = ingredient.name;
     ingredientsList.appendChild(ingredientItem);
@@ -209,47 +168,37 @@ export const createSecondDivElements = (pizzaObject) => {
 ingredientsDiv.appendChild(ingredientsTitle);
 ingredientsDiv.appendChild(ingredientsList);
 
-// append all divs to parent div
 descriptionDiv.appendChild(imgDiv);
 descriptionDiv.appendChild(pizzaDescDiv);
 descriptionDiv.appendChild(ingredientsDiv);
 console.log(secondDiv)
 
 secondDiv.appendChild(descriptionDiv)
-// const advetisementToAppend = advertisementDiv()
-// rootDiv.appendChild(advetisementToAppend)
-// createSecondDiv()
 return descriptionDiv;
 };
 
 export const advertisementDiv = () => {
-// container div element
 const advertisementDiv = document.createElement('div');
 advertisementDiv.classList.add('advertisement-div');
 
-// h3 element for title
 const titleElement = document.createElement('h3');
 titleElement.classList.add('advertisement-title');
 titleElement.textContent = 'Advertisement Title';
 advertisementDiv.appendChild(titleElement);
 
-// p element for paragraph
 const paragraphElement = document.createElement('p');
 paragraphElement.classList.add('advertisement-paragraph');
 paragraphElement.textContent = 'This is a sample advertisement.';
 advertisementDiv.appendChild(paragraphElement);
 
-//  button element
 const buttonElement = document.createElement('button');
 buttonElement.textContent = 'Click me';
 advertisementDiv.appendChild(buttonElement);
 
-//  img element for image
 const imageElement = document.createElement('img');
-imageElement.src = 'pizzaAdd.png'; // Replace with the actual image source
+imageElement.src = 'pizzaAdd.png';
 advertisementDiv.appendChild(imageElement);
 console.log(secondDiv)
-// Append the advertisementDiv to the DOM
 secondDiv.appendChild(advertisementDiv);
     return advertisementDiv
 }
@@ -257,11 +206,9 @@ secondDiv.appendChild(advertisementDiv);
 export const createthirdDivElements = (pizza) =>{
 
 function createOrderDiv(pizza) {
-    // Create the outer div
     const orderDiv = document.createElement('div');
     orderDiv.classList.add('order');
-  
-    // Create the name and price divs
+
     const nameDiv = document.createElement('div');
     nameDiv.classList.add('order-name');
     nameDiv.textContent = pizza.name;
@@ -269,16 +216,22 @@ function createOrderDiv(pizza) {
     const priceDiv = document.createElement('div');
     priceDiv.classList.add('order-price');
     priceDiv.textContent = '$' + pizza.price.toFixed(2);
+
   
-    // Create the plus and minus buttons
     const plusButton = document.createElement('button');
     plusButton.classList.add('plus-button');
     plusButton.textContent = '+';
     plusButton.addEventListener('click', () => {
-      pizza.quantity++;
+      if (!pizza.quantity){
+        pizza.quantity = 1;
+      }
+      else {
+        pizza.quantity++;
+      }
       quantityDiv.textContent = pizza.quantity;
       priceDiv.textContent = '$' + (pizza.price * pizza.quantity).toFixed(2);
     });
+  
   
     const minusButton = document.createElement('button');
     minusButton.classList.add('minus-button');
@@ -291,38 +244,18 @@ function createOrderDiv(pizza) {
       }
     });
   
-    // Create the quantity div
     const quantityDiv = document.createElement('div');
     quantityDiv.classList.add('order-quantity');
     quantityDiv.textContent = pizza.quantity;
   
-    // Add the name, price, quantity, plus, and minus elements to the order div
     orderDiv.appendChild(nameDiv);
     orderDiv.appendChild(priceDiv);
     orderDiv.appendChild(quantityDiv);
     orderDiv.appendChild(plusButton);
     orderDiv.appendChild(minusButton);
     thirdDiv.appendChild(orderDiv)
-    // Return the order div
     return orderDiv;
 
 }
  createOrderDiv(pizza)
 }
-
-
-
-
-
-// checkedAllergens.forEach(allergin => {
-    //   pizzaDivs.forEach(pizzaDiv => {
-    //     const pizzaName = pizzaDiv.classList[1];
-    //     const matchDivIfAllergenInPizza = pizzaName.map(name => pizzaData.forEach(piza => {
-    //       if(piza.allergens.includes(allergin)){
-    //         return piza.name
-    //       }
-           
-    //     }))
-    //     return pizzaDiv
-    //   })
-    // })
